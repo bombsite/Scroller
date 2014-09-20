e4cf3a47272dfbad17551d1212174559547a8444
Site Mao <site.mao1992@gmail.com>
2014-09-20 14:51:39 -0700

    added (unecessary)jquery lib and made basic checkbox

--- popup.js
+++ popup.js
@@ -1,16 +1,5 @@
+function changeHandler(){
+           alert("hi");
+        }
 
-var mutetab = {
- 
-    checkOptions: function(){
-        
-    } 
-    muteAll: function(){
-        
-    }
-
-};
-
-// Run our kitten generation script as soon as the document's DOM is ready.
-document.addEventListener('DOMContentLoaded', function () {
-  mutetab.muteAll();
-});
+document.getElementById("showAlert").addEventListener('change', changeHandler);

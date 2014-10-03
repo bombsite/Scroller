7ff415ad9c7fa7436f29abf1ee51431e035885e8
Site Mao <site.mao1992@gmail.com>
2014-10-02 21:26:33 -0700

    cleaned up some junk

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-First goal is to make something super stupid that tracks your scrolling. Increases a number when you scroll and simply displays "Total distance Scrolled: 42 miles"
+First goal is to make something super stupid that tracks your scrolling. Increases a number when you scroll and simply displays "Total distance Scrolled: 42 miles".
 Next goal is to make something even stupider that might be useful for someone someday, shift+scroll or alt+scroll to move through tabs.
 
--- popup.js
+++ popup.js
@@ -3,39 +3,9 @@ var background = chrome.extension.getBackgroundPage();
 
 function changeHandler(){
 
-chrome.bookmarks.create({'parentId': bookmarkNode.id,
-                               'title': 'Extension bookmarks'},
-                                                         function(newFolder) {
-                                                                 console.log("added folder: " + newFolder.title);
-                                                                   });
+});
       
     
-    
-    /*    var formTabs = new Array();
-    chrome.tabs.query({},function (tabs){
-        for(var i=0;i<tabs.length;i++){
-            formTabs[i]=tabs[i];
-        }
-        for(var i=0;i<formTabs.length;i++){
-            if(formTabs[i]!=null)
-                //background.console.log(formTabs[i].url);
-                addBookmark(formTabs[i]);
-            else
-                background.console.log("??" + i);
-        }
-    });
-*/
-
-}
-function addBookmark(input){
-    if(!input.title || !input.url)
-        return;
-
-
-
-    background.console.log(input.title);
-    background.console.log(input.url);
-}
 
 //Add a listener to know when the tool is disabled/enabled
 document.getElementById("showAlert").addEventListener('change', changeHandler);

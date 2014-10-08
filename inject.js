268acc3ca1f6ec18956ea132189388b79ae1d62f
Site Mao <site.mao1992@gmail.com>
2014-10-07 21:10:47 -0700

    basic storage working

--- inject.js
+++ inject.js
@@ -1,8 +1,32 @@
+var thevalue;
+
 $(window).bind('mousewheel', function(event) {
-    if (event.originalEvent.wheelDelta >= 0) {
-        console.log('Scroll up');
-    }
-    else {
-        console.log('Scroll down');
-    }
+    var changeValue;
+    chrome.storage.local.get('value',function(result){
+        changeValue = result.value;
+        console.log('init: '+changeValue);
+
+        if (event.originalEvent.wheelDelta >= 0) {
+            console.log('Scroll up');
+        }
+        else {
+            console.log('Scroll down');
+        }
+        changeValue+=1;
+        chrome.storage.local.set({'value': changeValue},function(){
+            console.log('final: '+changeValue);
+        });
+    });
+});
+
+$( document ).ready(function() {
+        chrome.storage.local.get('value',function(result){
+            console.log('init'+result.value);
+        });
+/* This code is unnecessary since the value is set to 0 by default
+        chrome.storage.local.set({'value': 0}, function() {
+           // Notify that we saved.
+            console.log('value set to 0');
+        });
+*/
 });
